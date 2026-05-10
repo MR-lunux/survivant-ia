d833ef5fe3efcac8289b2091eda18fbd5ad56f01
MR-lunux <mounds.insteps_0c@icloud.com>
2026-05-10 16:18:42 +0200

    feat(video/music): wire preset registry

---
+++ video/src/lib/score/presets/index.ts
@@ -0,0 +1,9 @@
+// src/lib/score/presets/index.ts
+import type { PresetDefinition, PresetName } from "../types";
+import { zimmerTense } from "./zimmer-tense";
+import { eightBitNostalgic } from "./8bit-nostalgic";
+
+export const PRESETS: Record<PresetName, PresetDefinition> = {
+  "zimmer-tense": zimmerTense,
+  "8bit-nostalgic": eightBitNostalgic,
+};
